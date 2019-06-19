58eae05ba6c449cb4194ab8e767400ea0d329554
lep31 <lep31@aber.ac.uk>
2019-06-19 18:14:16 +0100

    Created new project for second alphabet ordering algorithm

---
+++ AlphabetOrderingAlgorithm2/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String args[]){
+        String word = "aabdcaacdaabdbabaabcaacaacab";
+        String word2 = "a";
+        //String wordFasta = new ReadFastaToString();
+        char[] order = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        /*
+        Duval duval = new Duval();
+        System.out.println(word);
+        ExponentParikhVector epv = new ExponentParikhVector(word);
+        System.out.println(epv.toString());
+        System.out.println();
+        System.out.println(LyndonFactorizer.factorize(epv));
+        System.out.println();
+        System.out.println(duval.factor(order, word.toCharArray()));
+        System.out.println(LyndonFactorizer.factorize(word, false));
+        //--------------------------------algorithm 1
+
+        /
+        */
+        Mapping mapping = new Mapping();
+        String reorderedNoBacktrack = AlphabetReorderingAlg.reorder(word2, mapping, false);
+        String reorderedWithBacktrack = AlphabetReorderingAlg.reorder(word, mapping, true);
+        System.out.println(reorderedNoBacktrack);
+        System.out.println(reorderedWithBacktrack);
+        System.out.println(LyndonFactorizer.factorize(reorderedNoBacktrack, false));
+        System.out.println(LyndonFactorizer.factorize(word, false));
+    }
+}
