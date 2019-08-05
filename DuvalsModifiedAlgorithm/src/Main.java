2f4c5e9c90f0bfd219fc5693b24d6074af33349d
lep31 <lep31@aber.ac.uk>
2019-08-05 12:00:14 +0100

    Final version, for max and min

--- DuvalsModifiedAlgorithm/src/Main.java
+++ DuvalsModifiedAlgorithm/src/Main.java
@@ -1,9 +1,16 @@
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Main {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         String word = "aabdcaacdaabdbabaabcaacaacab";
         String word2 = "a";
         //String wordFasta = new ReadFastaToString();
@@ -21,6 +28,44 @@ public class Main {
         defaultDirectedGraph.addEdge("b", "c");
         System.out.println(defaultDirectedGraph);
         */
-        System.out.println(ModifiedDuval.factor(word));
+        Map<String, String> sequence = new ReadFastaToString().read(System.getProperty("user.dir") + "\\GCF_000009605.1_ASM960v1_protein.faa");
+        for(Map.Entry<String, String> entry : sequence.entrySet()){
+            System.out.println("ID: " + entry.getKey());
+            ArrayList<String> modifiedFact = ModifiedDuval.factor(entry.getValue(), true);
+            System.out.print("Previous factors: ");
+            System.out.println(LyndonFactorizer.factorize(entry.getValue(), false).size());
+            System.out.print("New factors: ");
+            System.out.println(modifiedFact.size());
+            System.out.print("Size of string: ");
+            System.out.println(entry.getValue().length());
+            System.out.println();
+        }
+        //ModifiedDuval.reorderSequence(sequence);
+        //modifiedVSfirstOrder();
+
+    }
+
+    public static void modifiedVSfirstOrder() {
+        Map<String, String> sequence = new ReadFastaToString().read(System.getProperty("user.dir") + "\\GCF_000009605.1_ASM960v1_protein.faa");
+        String pathname = System.getProperty("user.dir") + "\\ModifiedVS1stAppearance.txt";
+        File file = new File(pathname);
+        try {
+            PrintWriter printWriter = new PrintWriter(file);
+            Duval duval = new Duval();
+            for(Map.Entry<String, String> entry : sequence.entrySet()){
+                printWriter.println(entry.getKey());
+                ArrayList<String> modifiedFact = ModifiedDuval.factor(entry.getValue(), false);
+                ArrayList<String> firstOrdFact;
+                char[] order = duval.getOrderOfAppareance(entry.getValue());
+                firstOrdFact = duval.factor(order, entry.getValue().toCharArray());
+                printWriter.print(modifiedFact.size());
+                printWriter.print(",");
+                printWriter.println(firstOrdFact.size());
+                printWriter.flush();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- DuvalsModifiedAlgorithm/src/ModifiedDuval.java
+++ DuvalsModifiedAlgorithm/src/ModifiedDuval.java
@@ -1,12 +1,83 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public abstract class ModifiedDuval {
 
     private static PartialOrder partialOrder;
 
-    public static ArrayList<String> factor(String s){
-        partialOrder = new PartialOrder();
+    public static void reorderSequence(Map<String, String> sequence){
+        String pathname = System.getProperty("user.dir") + "\\GreedyAlgResults2.txt";
+        File file = new File(pathname);
+        try {
+            PrintWriter printWriter = new PrintWriter(file);
+            float totalCount = 0;
+            float optimizeCount = 0;
+            Map<Integer, Integer> amountOfFactors = new HashMap<>();
+            for(Map.Entry<String, String> entry : sequence.entrySet()){
+                totalCount += 1;
+                printWriter.println("-------------------------------------------------");
+                printWriter.print("ID: ");
+                printWriter.print(entry.getKey());
+                printWriter.println();
+                String proteinSeq = entry.getValue().toLowerCase() ;
+                ArrayList<String> proteinSeqFactors = LyndonFactorizer.factorize(proteinSeq, false);
+                printWriter.println("Factorization of the string with standard ordering (roman alphabet): ");
+                printWriter.println(proteinSeqFactors);
+                printWriter.print(" Number of factors: ");
+                int previousSize = proteinSeqFactors.size();
+                printWriter.println(previousSize);
+                ArrayList<String> proteinSeqFactReorder = ModifiedDuval.factor(proteinSeq, false);
+                printWriter.print("Factorization of the string with new ordering");
+                printWriter.println(proteinSeqFactReorder);
+                printWriter.print("Number of factors: ");
+                int newSize = proteinSeqFactReorder.size();
+                printWriter.println(newSize);
+                printWriter.println("-------------------------------------------------");
+                if(previousSize > newSize){
+                    optimizeCount += 1;
+                }else{
+                    System.out.print("ID: ");
+                    System.out.println(entry.getKey());
+                    System.out.print("Previous factors: ");
+                    System.out.println(previousSize);
+                    System.out.print("New factors: ");
+                    System.out.println(newSize);
+                }
+                if(!amountOfFactors.containsKey(newSize)){
+                    amountOfFactors.put(newSize, 1);
+                }else{
+                    amountOfFactors.replace(newSize, amountOfFactors.get(newSize) + 1);
+                }
+                printWriter.flush();
+            }
+            printWriter.print("Total sequences: ");
+            printWriter.println(totalCount);
+            printWriter.print("Amount of strings optimized: ");
+            printWriter.print(optimizeCount);
+            printWriter.print("; Percentage: ");
+            printWriter.print((optimizeCount/totalCount)*100);
+            printWriter.println("%");
+            for(Map.Entry<Integer, Integer> instances : amountOfFactors.entrySet()){
+                printWriter.print("Amount of sequences with ");
+                printWriter.print(instances.getKey());
+                printWriter.print(" factor(s): ");
+                printWriter.print((instances.getValue()/totalCount)*100);
+                printWriter.println("%");
+                printWriter.flush();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<String> factor(String s, boolean max){
+        partialOrder = new PartialOrder(max);
         int h = 0;
         ArrayList<ModCharacter> chars = new ArrayList<>();
         for(Character character : s.toCharArray()){
@@ -36,7 +107,7 @@ public abstract class ModifiedDuval {
                 h += j - i;
             }
         }
-        System.out.println(partialOrder);
+        System.out.println("Number of nodes: " + partialOrder.getNumOfNodes());
         return resultList;
     }
     private static class ModCharacter implements Comparable{
